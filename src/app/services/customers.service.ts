0e8692b6300fd339f5dca2d6bf97896eea7ad51c
Mohamed Adel <mohamedadelsa4@gmail.com>
2022-05-10 11:44:44 +0200

    anything

---
+++ src/app/services/customers.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomersService {
+
+  httpOption;
+  constructor(private httpClient: HttpClient) {
+    this.httpOption = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+        // ,Authorization: 'my-auth-token'
+      })
+    };
+  }
+
+  getAllCustomers(){
+    return this.httpClient.get(`${environment.APIURL}/login`)
+  }
+}
