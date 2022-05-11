f5af3eb0a4c69df74856aa0d4751ae38200e2bc6
Mohamed Adel <mohamedadelsa4@gmail.com>
2022-05-11 14:46:35 +0200

    finsh customer details UI with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,23 @@ import { CustomerDetailsComponent } from './customer/customer-details/customer-d
 import { CustomersService } from './services/customers.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule }   from '@angular/forms';
+
+//angular forms
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//material modules
+import { MatSliderModule } from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,11 +36,20 @@ import { FormsModule }   from '@angular/forms';
     BrowserModule,
     HttpClientModule ,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatIconModule
 
   ],
   providers: [
-    CustomersService
+    CustomersService,
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}
   ],
   bootstrap: [AppComponent]
 })
--- src/app/customer/customer-details/customer-details.component.ts
+++ src/app/customer/customer-details/customer-details.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-customer-details',
   templateUrl: './customer-details.component.html',
   styleUrls: ['./customer-details.component.scss']
 })
 export class CustomerDetailsComponent implements OnInit {
-  newCustomer:string=''
-  constructor() { }
+  // email = new FormControl('', [Validators.required, Validators.email]);
+  myForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.myForm = this.fb.group({
+      customerName: ['', [
+        Validators.required, Validators.minLength(3), Validators.maxLength(20)
+      ]],
+      idNumber: ['', [
+        Validators.required, Validators.minLength(14), Validators.maxLength(14)
+      ]],
+      phone: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+      city:['', [Validators.required]],
+      address:['',[Validators.required, Validators.minLength(5), Validators.maxLength(30)]]
+    });
   }
 
+  getErrorMessage() {
+    if (this.customerName!.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.customerName!.hasError('') ? 'Not a valid email' : '';
+  }
+
+  get customerName() {
+    return this.myForm.get('customerName')
+  }
+
+  get idNumber() {
+    return this.myForm.get('idNumber')
+  }
+
+  get phone() {
+    return this.myForm.get('phone')
+  }
+
+  get city() {
+    return this.myForm.get('city')
+  }
+
+  get address() {
+    return this.myForm.get('address')
+  
+  }
+
+  getControlError(control:any) {
+    console.log(control);
+    return true;
+    
+  }
 }
