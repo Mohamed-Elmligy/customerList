8f763e4606c1841b36c1fe4946b3c3af5faaf97c
Mohamed Adel <mohamedadelsa4@gmail.com>
2022-05-15 15:12:05 +0200

    create customer item details form & validation

--- src/app/customer-item/customer-item-details/customer-item-details.component.ts
+++ src/app/customer-item/customer-item-details/customer-item-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-item-details',
@@ -6,8 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-item-details.component.scss']
 })
 export class CustomerItemDetailsComponent implements OnInit {
-  value = 'Clear me';
-  constructor() { }
+
+  //#region variavles
+
+  myForm!: FormGroup;
+
+  //#endregion
+
+  //#region constructor
+  constructor(
+    private formBuilder: FormBuilder,
+  )
+   {
+     this.initForm()
+    }
+    //#endregion
+
+    //#region init form
+  initForm(){
+    this.myForm = this.formBuilder.group({
+      item_name: ['', [Validators.required]],
+      item_code: ['', [Validators.required]],
+      item_price: ['', [Validators.required]],
+      item_quantity: ['', [Validators.required]],
+      discription: ['', [Validators.required]]
+    })
+  }
+
+
+  getErrorMessage(t: any) {
+    if (t?.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return t?.hasError('') ? 'Not a valid' : '';
+  }
+
+
+  get myFormControls() {
+    return this.myForm.controls;
+  }
+
+  //#endregion
 
   ngOnInit(): void {
   }
